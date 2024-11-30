3cb422fcf92cb4b823dd1de720a75d91a3a945a8
xuncheng-001 <205455603@qq.com>
2024-11-30 13:49:49 +0800

    Improve the some files and add some new files.

--- include/hero_chassis_controller/cqc_lastwork_controller.h
+++ include/hero_chassis_controller/cqc_lastwork_controller.h
@@ -5,21 +5,21 @@
 #ifndef RM_DESCRIPTION_CQC_LASTWORK_CONTROLLER_H
 #define RM_DESCRIPTION_CQC_LASTWORK_CONTROLLER_H
 
-#include <controller_interface/controller_interface.h>
+#include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 
 namespace cqc_lastwork_controller
 {
 class CqcLastworkController : public controller_interface::Controller<hardware_interface::EffortJointInterface>
-//继承controller_interface::Controller，并且是和hardware_interface::EffortJointInterface关联，也就是控制力矩的
+
 {
 public:
     CqcLastworkController() = default;
     ~CqcLastworkController()override = default;
 
-    bool init(hardware_interface::EffortJonitInterface* effort_joint_interface, ros::NodeHandle& root_nh,ros::Nodehandle& controller_nh )override;
+    bool init(hardware_interface::EffortJointInterface* effort_joint_interface, ros::NodeHandle& root_nh,ros::NodeHandle& controller_nh )override;
 
-    void update(const ros::Time$ time ,const ros::Duration& period)override;
+    void update(const ros::Time& time ,const ros::Duration& period)override;
 
     hardware_interface::JointHandle front_left_joint, front_right_joint, back_left_joint, back_right_joint;
 };
--- src/cqc_lastwork_controller.cpp
+++ src/cqc_lastwork_controller.cpp
@@ -3,8 +3,24 @@
 //
 #include <pluginlib/class_list_macros.hpp>
 #include "hero_chassis_controller/cqc_lastwork_controller.h"
-namespace cqc_lastwork_controller
-{
-    bool
+namespace cqc_lastwork_controller{
+    bool CqcLastworkController::init(hardware_interface::EffortJointInterface* effort_joint_interface,
+                                     ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh){
 
+        front_left_joint = effort_joint_interface->getHandle("left_front_wheel_joint");
+        front_right_joint = effort_joint_interface->getHandle("right_front_wheel_joint");
+        back_left_joint = effort_joint_interface->getHandle("left_back_wheel_joint");
+        back_right_joint = effort_joint_interface->getHandle("right_back_wheel_joint");
+
+        return true;
+    }
+
+    void CqcLastworkController::update(const ros::Time& time, const ros::Duration& period)
+    {
+        front_left_joint.setCommand(200);
+        front_right_joint.setCommand(200);
+        back_left_joint.setCommand(200);
+        back_right_joint.setCommand(200);
+    }
+    PLUGINLIB_EXPORT_CLASS(cqc_lastwork_controller::CqcLastworkController,controller_interface::ControllerBase)
 }
