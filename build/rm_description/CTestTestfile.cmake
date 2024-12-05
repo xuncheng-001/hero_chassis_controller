dd696912028b52d0633388b665de8eb604ee6314
xuncheng-001 <205455603@qq.com>
2024-12-05 12:42:59 +0800

    Delet unuseful files.

--- build/rm_description/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xuncheng/lastwork/src/rm_description_for_task
-# Build directory: /home/xuncheng/lastwork/build/rm_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/simple_chassis_controller/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xuncheng/lastwork/src/simple_chassis_controller
-# Build directory: /home/xuncheng/lastwork/build/simple_chassis_controller
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
