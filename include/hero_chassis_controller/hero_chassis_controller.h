b7aa1426a478ad1f7d43073f95759f7006007844
xuncheng-001 <205455603@qq.com>
2024-12-05 12:39:45 +0800

    update the files.

--- include/hero_chassis_controller/hero_chassis_controller.h
+++ include/hero_chassis_controller/hero_chassis_controller.h
@@ -1,37 +1,32 @@
 //
-// Created by xuncheng on 24-11-28.
+// Created by qiayuan on 2/6/21.
 //
 
-#ifndef RM_DESCRIPTION_CQC_LASTWORK_CONTROLLER_H
-#define RM_DESCRIPTION_CQC_LASTWORK_CONTROLLER_H
+#ifndef SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
+#define SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
 
 #include <controller_interface/controller.h>
 #include <hardware_interface/joint_command_interface.h>
 
+namespace hero_chassis_controller{
 
-namespace hero_chassis_controller
-{
 class CqcLastWorkController : public controller_interface::Controller<hardware_interface::EffortJointInterface>
-
 {
 public:
-    CqcLastWorkController() = default;
-    ~CqcLastWorkController()override = default;
+  CqcLastWorkController() = default;
+  ~CqcLastWorkController() override = default;
 
-    bool init(hardware_interface::EffortJointInterface* effort_joint_interface, ros::NodeHandle& root_nh,ros::NodeHandle& controller_nh )override;
+  bool init(hardware_interface::EffortJointInterface* effort_joint_interface, ros::NodeHandle& root_nh,
+            ros::NodeHandle& controller_nh) override;
 
-    void update(const ros::Time& time ,const ros::Duration& period)override;
+  void update(const ros::Time& time, const ros::Duration& period) override;
 
-    hardware_interface::JointHandle front_left_joint, front_right_joint, back_left_joint, back_right_joint;
+  hardware_interface::JointHandle front_left_joint_, front_right_joint_, back_left_joint_, back_right_joint_;
 
 private:
-    int state_;
-    ros::Time last_time_;
+  int state_{};
+  ros::Time last_change_;
 };
-}
+}  //  namespace simple_chassis_controller
 
-
-
-
-
-#endif //RM_DESCRIPTION_CQC_LASTWORK_CONTROLLER_H
+#endif  // SIMPLE_CHASSIS_CONTROLLER_SIMPLE_CHASSIS_CONTROLLER_H
--- src/hero_chassis_controller.cpp
+++ src/hero_chassis_controller.cpp
@@ -1,29 +1,31 @@
 //
-// Created by xuncheng on 24-11-28.
+// Created by qiayuan on 2/6/21.
 //
-#include <pluginlib/class_list_macros.hpp>
+
 #include "hero_chassis_controller/hero_chassis_controller.h"
-namespace hero_chassis_controller
+#include <pluginlib/class_list_macros.hpp>
+#include <controller_interface/controller.h>
+#include <hardware_interface/joint_command_interface.h>
+
+
+namespace hero_chassis_controller{
+bool CqcLastWorkController::init(hardware_interface::EffortJointInterface* effort_joint_interface,
+                                   ros::NodeHandle& root_nh, ros::NodeHandle& controller_nh)
 {
-    bool CqcLastWorkController::init(hardware_interface::EffortJointInterface* effort_joint_interface,
-                                     ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh){
+  front_left_joint_ = effort_joint_interface->getHandle("left_front_wheel_joint");
+  front_right_joint_ = effort_joint_interface->getHandle("right_front_wheel_joint");
+  back_left_joint_ = effort_joint_interface->getHandle("left_back_wheel_joint");
+  back_right_joint_ = effort_joint_interface->getHandle("right_back_wheel_joint");
 
-        front_left_joint = effort_joint_interface->getHandle("left_front_wheel_joint");
-        front_right_joint = effort_joint_interface->getHandle("right_front_wheel_joint");
-        back_left_joint = effort_joint_interface->getHandle("left_back_wheel_joint");
-        back_right_joint = effort_joint_interface->getHandle("right_back_wheel_joint");
-
-        return true;
-    }
-
-
-
-
-    void CqcLastWorkController::update(const ros::Time& time, const ros::Duration& period)
-    {
-    front_left_joint.setCommand(2.0);
-    }
-
-
-   PLUGINLIB_EXPORT_CLASS(hero_chassis_controller::CqcLastWorkController,controller_interface::ControllerBase)
+  return true;
 }
+
+void CqcLastWorkController::update(const ros::Time& time, const ros::Duration& period)
+{
+
+  front_left_joint_.setCommand(2.0 );
+
+}
+
+PLUGINLIB_EXPORT_CLASS(hero_chassis_controller::CqcLastWorkController,  controller_interface::ControllerBase)
+}  // namespace simple_chassis_controller
